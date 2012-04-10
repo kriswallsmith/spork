cd679f3bfa6784874b61532a71d974c79d3adc91
Kris Wallsmith <kris.wallsmith@gmail.com>
2012-04-10 08:49:17 -0700

    added license file and headers

--- src/Spork/Deferred/DeferredInterface.php
+++ src/Spork/Deferred/DeferredInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Spork package, an OpenSky project.
+ *
+ * (c) 2010-2011 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Spork\Deferred;
 
 interface DeferredInterface extends PromiseInterface
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Spork package, an OpenSky project.
+ *
+ * (c) 2010-2011 OpenSky Project Inc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 if (!$loader = @include __DIR__.'/../vendor/.composer/autoload.php') {
     die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
         'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
