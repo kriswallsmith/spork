9ce3d44921c4038843d395f7ed3e26e84b743ced
Kris Wallsmith <kris.wallsmith@gmail.com>
2012-04-12 18:01:07 -0700

    fixed copyright year

--- src/Spork/Deferred/DeferredInterface.php
+++ src/Spork/Deferred/DeferredInterface.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Spork package, an OpenSky project.
  *
- * (c) 2010-2011 OpenSky Project Inc
+ * (c) 2012 OpenSky Project Inc
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
