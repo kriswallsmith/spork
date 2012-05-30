99e822c3560df6a72977c63643e3b962be02bec9
Kris Wallsmith <kris.wallsmith@gmail.com>
2012-05-30 05:35:31 -0700

    Merge commit '7e57d26'
    closes #4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,8 +9,7 @@
  * file that was distributed with this source code.
  */
 
-spl_autoload_register(function($class)
-{
+spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Spork\\Test\\') && file_exists($file = __DIR__.'/'.str_replace('\\', '/', $class).'.php')) {
         require_once $file;
     } elseif (0 === strpos($class, 'Spork\\') && file_exists($file = __DIR__.'/../src/'.str_replace('\\', '/', $class).'.php')) {
