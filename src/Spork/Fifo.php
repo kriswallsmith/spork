9aa8ec9d93c2a8fd0eaf3b4f0207bfeff0502803
Kris Wallsmith <kris.wallsmith@gmail.com>
2012-05-31 10:22:01 -0700

    ignore fifo already exists errors

--- src/Spork/Fifo.php
+++ src/Spork/Fifo.php
@@ -34,8 +34,8 @@ class Fifo
         foreach (array_combine($directions, $modes) as $direction => $mode) {
             $fifo = realpath(sys_get_temp_dir()).'/spork'.$pid.'.'.$direction;
 
-            if (!file_exists($fifo) && !posix_mkfifo($fifo, 0600)) {
-                throw new ProcessControlException('Unable to create FIFO');
+            if (!file_exists($fifo) && !posix_mkfifo($fifo, 0600) && 17 !== $error = posix_get_last_error()) {
+                throw new ProcessControlException(sprintf('Error while creating FIFO: %s (%d)', posix_strerror($error), $error));
             }
 
             $this->$mode = fopen($fifo, $mode[0]);
