34b518ebd0f4e9e76bf2ef586d9b35fa1acc51b2
Kris Wallsmith <kris.wallsmith@gmail.com>
2013-03-20 21:42:22 -0700

    updated license headers

--- src/Spork/EventDispatcher/EventDispatcherInterface.php
+++ src/Spork/EventDispatcher/EventDispatcherInterface.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Spork, an OpenSky project.
  *
- * (c) 2012 OpenSky Project Inc
+ * (c) OpenSky Project Inc
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
